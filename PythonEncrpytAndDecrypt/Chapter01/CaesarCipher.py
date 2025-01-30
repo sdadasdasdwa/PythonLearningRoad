dec6024554d4d405bef7709e064cc85312fa3a3a
Ricardo Pan at MacBook <piybeoycmcsi@gmail.com>
2025-01-30 18:55:23 +0800

    [PythonCipher] initially finish Caesar Cipher algorithm 2025-01-30 [Reason] [Method] [Notes]

--- PythonEncrpytAndDecrypt/Chapter01/CaesarCipher.py
+++ PythonEncrpytAndDecrypt/Chapter01/CaesarCipher.py
@@ -1,6 +1,53 @@
+import time
 
-def a():
 
-    pass
+def diplayIntro():
+    print("There you will choose method between encryption and decryption")
+    print()
 
-original = 'The Secret Password is Rosebud'
+
+def chooseCave():
+    cave = ""
+    while cave != "1" and cave != "2":
+        print("which cave you want to choose? (1 or 2)")
+        cave = input()
+    return cave
+
+
+def checkCave(number, text):
+    print("you have made your choice")
+    time.sleep(2)
+    result = ""
+    if number == "1":
+        print("this mode is encrypt")
+        shift = input("please input encrypt shift number: ")
+        result = caesar_cipher(text, int(shift), "encrypt")
+        print("encrypt result : {}".format(result))
+    elif number == "2":
+        print("this mode is decrypt")
+        shift = input("please input decrypt shift number: ")
+        result = caesar_cipher(text, int(shift), "decrypt")
+        print("decrypt result : {}".format(result))
+
+
+def caesar_cipher(text, shift, mode="encrypt"):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            shift_amount = shift if mode == "encrypt" else -shift
+            start = ord("A") if char.isupper() else ord("a")
+            result += chr((ord(char) - start + shift_amount) % 26 + start)
+        else:
+            result += char  # Non-alphabetic characters remain unchanged
+    return result
+
+
+if __name__ == "__main__":
+
+    original = "The Secret Password is Rosebud"
+    encryptAgain = "yes"
+    while encryptAgain == "yes" or encryptAgain == "y":
+        diplayIntro()
+        numberChosen = chooseCave()
+        checkCave(numberChosen, original)
+        encryptAgain = input("Do you want to play again?")
