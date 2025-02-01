23e34216fee6a886bb018d9b1baa79f3ae760bab
Ricardo Pan at MacBook <piybeoycmcsi@gmail.com>
2025-02-01 18:13:29 +0800

    [PythonCipher] permutation cipher

--- PythonEncrpytAndDecrypt/Chapter01/CaesarCipherSecond.py
+++ PythonEncrpytAndDecrypt/Chapter01/CaesarCipherSecond.py
@@ -1,30 +1,32 @@
 # message to encrypt or decrypt
 # message = "This is my secret message."
-message = 'Tvi6ki6kz!k6sq5s7kzs66ousn'
+message = 'ZqH8sl5HtqHTH4s3lyvH5zH5spH4t pHzqHlH3l5K'
 
 # key
-key = 13
+# key = 13
 
 # mode
 # mode = "encrypt"
 mode = "decrypt"
 
 # symbols which might be encrypted
-symbols = "abcdefghjklmnopqrstuvwxyz1234567890 !?."
+symbols = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 !?."
 
 # store the encrypt or decrypt result
-translate = ""
 
-for symbol in message:
 
-    if symbol in symbols:
+for key in range(len(symbols)):
+    translate = ""
+    for symbol in message:
+        if symbol in symbols:
+            symbolIndex = symbols.find(symbol)
+            shift = key if mode == "encrypt" else -key
+            symbolIndex = (shift + symbolIndex) % (len(symbols))
+            translate += symbols[symbolIndex]
+        else:
+            translate += symbol
 
-        symbolIndex = symbols.find(symbol)
-        shift = key if mode == "encrypt" else -key
-        symbolIndex = (shift + symbolIndex) % (len(symbols))
-        translate += symbols[symbolIndex]
-    else:
-        translate += symbol
+    # print result
+    print('Key # %s: %s' % (key, translate))
 
-# print result
-print(translate)
+
---
+++ PythonEncrpytAndDecrypt/Chapter7/ReadMe07.md
@@ -0,0 +1,18 @@
+## Permutation Cipher
+
+
+**Substitution Cipher** 
+
+This involves replacing each letter or symbol in the plaintext with another letter or symbol.
+
+A common example is the Caesar cipher, where each letter is shifted by a fixed number.
+
+**Permutation Cipher** 
+
+ This involves rearranging the order of the letters or symbols in the plaintext, rather than 
+ 
+ substituting them. A common example is the columnar transposition cipher, where characters 
+ 
+ are rearranged based on a key.
+
+ 
