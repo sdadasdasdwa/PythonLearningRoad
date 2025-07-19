5ebd4aa81e4440a1e304017cdce7a8e1fc79847e
Mr.CG <wrightzahi5581@outlook.com>
2025-07-19 23:16:57 +0800

    [Spider] 使用request库发起请求

---
+++ Spider/Chapter01/request_01.py
@@ -0,0 +1,11 @@
+import requests
+
+url = 'https://www.sogou.com/web?query=周杰伦'
+headers = {
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36"
+}
+
+resp = requests.get(url,headers=headers)
+
+print(resp)
+print(resp.text)
