be721069a7c4c5bd7d65003a878e13d158ec29fd
Mr.CG <wrightzahi5581@outlook.com>
2025-07-19 23:31:32 +0800

    [Spider] post方式发送请求

---
+++ Spider/Chapter01/requests_02.py
@@ -0,0 +1,12 @@
+import requests
+
+url = 'https://fanyi.baidu.com/sug'
+query = input("请输入查询单词: ")
+dat = {
+    "kw" : query
+}
+
+resp = requests.post(url,data=dat)
+print(resp.json()) # 将服务器返回的内容直接处理成json
+
+
