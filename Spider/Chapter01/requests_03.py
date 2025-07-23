743dfe62275072990ae50c132a1a3a7169755456
Mr.CG <wrightzahi5581@outlook.com>
2025-07-23 23:11:59 +0800

    [Spider] requests爬取豆瓣排行榜

--- Spider/Chapter01/requests_03.py
+++ Spider/Chapter01/requests_03.py
@@ -1,1 +1,22 @@
-import request
+import requests
+
+url = 'https://movie.douban.com/j/chart/top_list'
+
+params = {
+    "type" : "24",
+    "interval_id" : "100:90",
+    "action":"",
+    "start":0,
+    "limit":20,
+}
+
+headers = {
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36"
+}
+
+resp = requests.get(url=url, params=params,headers=headers)
+
+# print(resp.request.url)
+# print(resp.request.headers)
+# print(resp.text)
+print(resp.json())
