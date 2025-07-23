1f59d532e278ea983df9a75f515deec2500984e0
Mr.CG <wrightzahi5581@outlook.com>
2025-07-23 22:51:23 +0800

    使用f string方式将变量塞进字符串里面

--- Spider/Chapter01/requests_01.py
+++ Spider/Chapter01/requests_01.py
@@ -1,6 +1,7 @@
 import requests
 
-url = 'https://www.sogou.com/web?query=周杰伦'
+query = input("请输入一个你喜欢的明星")
+url = f'https://www.sogou.com/web?query={query}'
 headers = {
     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36"
 }
