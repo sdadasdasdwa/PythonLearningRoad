83004d727bdf4adda157c3684185a2d0c0d14cde
Mr.CG <wrightzahi5581@outlook.com>
2025-08-14 00:26:47 +0800

    [AppiumTest] 修改真机示例参数，结果运行成功 1. Appium2.x版本默认URL没有/wd/hub 2. 确保安装安卓设备适配uiautomator2驱动 appium driver install uiautomator2 3. 获取安卓设备版本 adb shell getprop ro.build.version.release 4. 寻找设备当前页面及活动 adb shell dumpsys window | findstr mCurrentFocus 5. 记得配置ANDROID_HOME或ANDROID_SDK_ROOT系统环境变量

--- AppiumTest/test.py
+++ AppiumTest/test.py
@@ -1,16 +1,22 @@
 from appium import webdriver
+from appium.options.android import UiAutomator2Options
 import time
 
 desired_caps = {
     "platformName": "Android",         # 平台
-    "platformVersion": "13",           # 安卓版本（adb shell getprop ro.build.version.release）
-    "deviceName": "MyAndroid",         # 设备名（adb devices 查）
-    "appPackage": "com.android.calculator2",  # 应用包名
-    "appActivity": ".Calculator",      # 启动的活动
-    "noReset": True                    # 不重置应用数据
+    "platformVersion": "14",           # 安卓版本（adb shell getprop ro.build.version.release）
+    "deviceName": "c813a9aa",         # 设备名（adb devices 查）
+    "appPackage": "cn.damai",  # 应用包名
+    "appActivity": ".homepage.MainActivity",      # 启动的活动
+    "noReset": True,                    # 不重置应用数据
+    "automationName": "UiAutomator2"
 }
 
-driver = webdriver.Remote("http://127.0.0.1:4723/wd/hub", desired_caps)
+options = UiAutomator2Options().load_capabilities(desired_caps)
 
-time.sleep(3)  # 等待应用启动
+
+driver = webdriver.Remote("http://127.0.0.1:4723", options=options)
+
+# print(driver.page_source)
+time.sleep(3)
 driver.quit()
