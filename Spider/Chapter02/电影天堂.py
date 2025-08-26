e8e403ace1cefe805e69ab9413e7bc550914e4c2
Mr.CG <wrightzahi5581@outlook.com>
2025-08-27 00:07:22 +0800

    [Spider] 电影天堂示例爬虫完成

--- Spider/Chapter02/电影天堂.py
+++ Spider/Chapter02/电影天堂.py
@@ -1,5 +1,8 @@
 import requests
 import re
+import time
+import random
+import csv
 
 def fetch_html(url, headers):
     """获取网页源码"""
@@ -23,11 +26,25 @@ def parse_html(html):
     for it in patternhref.finditer(hotmovie):
         results.append(it.group("href"))
     return results
+
+def parse_children_html(children_html):
+    """"用正则解析子页面信息"""
+    pattern = re.compile(r'◎片　　名　(?P<moviename>.*?)<br />'
+                         r'.*?上映日期　(?P<moviedate>.*?)<br />'
+                         r'.*?<td style="WORD-WRAP: break-word".*?<a href="(?P<movieadress>.*?)">',re.S)
+    results = []
+    for it in pattern.finditer(children_html):
+        dic = it.groupdict()
+        results.append(dic)
+    return results
+
+def save_to_csv(filename, rows):
     
-
-def save_to_csv():
-    pass
-
+    with open(filename, mode="w", newline='', encoding='utf-8-sig') as f:
+        writer = csv.writer(f)
+        writer.writerow(["电影名", "年份", "网址"])  # 表头
+        for row in rows:
+            writer.writerow(row.values())
 
 def main():
     url = "https://www.dytt8899.com/"
@@ -40,9 +57,25 @@ def main():
     # 存储热门电影栏目每条网页网址
     adresses = []
     adresses.extend(parse_html(html))
-    print(adresses)
+    # print(adresses)
     
-    save_to_csv()
+    # 对分网页地址进行逐条获取源代码
+    all_movies = []
+    for adress in adresses:
+        child_href = url + adress.strip("/")
+        child_html = fetch_html(child_href,headers)
+        if child_html:
+            movieInfo = parse_children_html(child_html)
+            all_movies.extend(movieInfo)
+            print(f"[INFO] 子网页 {child_href} 获取成功")
+        else:
+            print(f"[WARN] 子网页 {child_href} 获取失败")
+        # 防反爬，随机等待
+        delay = random.uniform(1, 3)
+        print(f"[INFO] 等待 {delay:.2f} 秒后继续爬取...")
+        time.sleep(delay)
+        
+    save_to_csv('./Spider/Chapter02/MovieHeaven.csv',all_movies)
 
 if __name__ == "__main__":
     main()
